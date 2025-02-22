8aefbac7a6cb973a58ab226ae40b8eb0bd46ca12
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-02-23 01:36:30 +0530

    .

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <input type="text" placeholder="Message..."></input>
+        <button>Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
