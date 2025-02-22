09a990a40388aab6433070e2ab403bb0aa9537fe
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-02-23 01:47:22 +0530

    .

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
+import { useEffect } from "react";
 import "./App.css";
 
 const App = () => {
+  function sendMessage() {
+    ws.send("ping");
+  }
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8000");
+
+    ws.onmessage = (ev) => {
+      alert(ev.data);
+    };
+  }, []);
   return (
     <div>
       <div>
         <input type="text" placeholder="Message..."></input>
-        <button>Send</button>
+        <button onClick={sendMessage}>Send</button>
       </div>
     </div>
   );
